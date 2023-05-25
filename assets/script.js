ca834a85f797932206faba7adc0e7017cbaa2386
Augustus1110 <donahi22@gmail.com>
2023-05-25 00:12:11 -0700

    Added comments to Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
+//Created variables matching the html code and assigned them the value of the element with their ID 
+
 var timer = document.getElementById("timer");
 var time = document.getElementById("time");
-var start_screen = document.getElementById("start_screen");
+var startScreen = document.getElementById("start_screen");
+var start = document.getElementById("start");
 var startButton = document.getElementById("start_button");
 var questionsDiv = document.getElementById("questions_div");
 var title = document.getElementById("title");
@@ -9,3 +12,9 @@ var endScreen= document.getElementById("end_screen");
 var finalScore= document.getElementById("final_score");
 var initials = document.getElementById("initials");
 var submitButton = document.getElementById("submit_button");
+
+//I created these additional variables as I wrote my javascript and needed them to run my code
+
+
+
+//When Start Quiz button is clicked, the Start Screen becomes hidden, the timer starts counting down and the questions appear
