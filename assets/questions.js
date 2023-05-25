df078af0d2649de10ec56dbe7a6151eab16525f7
Augustus1110 <donahi22@gmail.com>
2023-05-24 17:44:26 -0700

    Added questions in js file

---
+++ assets/questions.js
@@ -0,0 +1,45 @@
+//These are the 5 questions that will be used on the quiz
+
+let questions = [
+    { 
+        Number: 1,
+        question: "Commonly used data types do NOT include _____ .", answer: "alerts", choices: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ]
+    },
+    { Number: 2,
+        question: "The condition in an if / else statement is enclosed within _____ .", answer: "parentheses", choices: [
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "square brackets"
+        ]
+    },
+    { Number: 3,
+        question: "In JavaScript, Arrays can be used to store _____ .", answer: "all of the above", choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ]
+    },
+    { Number: 4,
+        question: "String values must be enclosed within ____ when being assigned to variables?", answer: "quotes", choices: [
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "commas"
+        ]
+    },
+    { Number: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is _____ .", answer: "console.log", choices: [
+            "JavaScript",
+            "terminal/ bash",
+            "console.log",
+            "for loops"
+        ]
+    },
+]
